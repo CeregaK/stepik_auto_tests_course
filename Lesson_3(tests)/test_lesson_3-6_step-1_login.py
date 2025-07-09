b67a2afdb0639c2ca5ff3ff2d654dcdc074c33f9
Sergei <Cerega.kv@yandex.ru>
2025-07-09 23:56:07 +0300

    add some test files in Lesson_3(tests)

--- Lesson_3(tests)/test_lesson_3-6_step-1_login.py
+++ Lesson_3(tests)/test_lesson_3-6_step-1_login.py
@@ -21,7 +21,7 @@ def test_guest_should_see_login_link(browser):
     element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'[href="/lesson/236895/step/1?auth=login"]')))
     browser.execute_script("arguments[0].scrollIntoView();", element)
     element.click()
-    browser.find_element(By.CSS_SELECTOR, '#id_login_email').send_keys('cerega.kv@yandex.ru')
+    browser.find_element(By.CSS_SELECTOR, '#id_login_email').send_keys('c___-----____u')
     time.sleep(2)
     browser.find_element(By.CSS_SELECTOR, '#id_login_password').send_keys('d---____---g')
     time.sleep(2)
