e767c0baef3c72c9f3369cae4a1a79d361c17b91
Cerega.K <cerega.kv@yandex.ru>
2025-10-29 18:27:26 +0300

    Delete alert_script

--- alert_script_execute.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-import time
-browser = webdriver.Chrome()
-browser.execute_script("document.title='Мое название страницы';alert('RМы поменяли название страницы!');")
-time.sleep(10)
-browser.quit()
